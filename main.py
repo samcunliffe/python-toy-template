97269213b119444851810e4cab3c1252874de024
Sam Cunliffe <samcunliffe@users.noreply.github.com>
2023-10-06 16:44:34 +0100

    Make main.py nonempty so we get coverage.

---
+++ main.py
@@ -0,0 +1,9 @@
+import typer
+
+
+def main() -> None:
+    ...
+
+
+if __name__ == "__main__":
+    typer.run(main)
--- tests/test_nada.py
+++ tests/test_nada.py
@@ -1,2 +1,6 @@
+from main import main
+
+
 def test_nothing():
+    main()
     assert True is True
